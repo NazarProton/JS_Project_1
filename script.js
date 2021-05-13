eaee96df9f13d2edc4452b42ec1eee77683dd69b
NP <nproton2012@gmail.com>
2021-05-13 22:33:56 +0300

    NP

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let mainValue = document.querySelector(".js-main-numbers"),
+  key = document.querySelector(".description1"),
+  locationRef = document.querySelector(".description2"),
+  which = document.querySelector(".description3"),
+  code = document.querySelector(".description4");
+
+window.addEventListener("keydown", (event) => {
+  mainValue.textContent = event.keyCode;
+  key.textContent = event.key;
+  locationRef.textContent = event.location;
+  which.textContent = event.which;
+  code.textContent = event.code;
+
+  if (event.code === "Space") {
+    key.textContent = "spase char";
+  }
+});
